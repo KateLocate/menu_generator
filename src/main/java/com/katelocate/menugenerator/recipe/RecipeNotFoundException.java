a327dc2ba8a9c9491ac8fc8f0b5640f198a1ace5
katelocate <katelocat3@gmail.com>
2024-06-12 17:59:33 +0200

    Add a RecipeNotFoundException class.

---
+++ src/main/java/com/katelocate/menugenerator/recipe/RecipeNotFoundException.java
@@ -0,0 +1,11 @@
+package com.katelocate.menugenerator.recipe;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class RecipeNotFoundException extends RuntimeException {
+    public RecipeNotFoundException() {
+        super("Recipe not found.");
+    }
+}
