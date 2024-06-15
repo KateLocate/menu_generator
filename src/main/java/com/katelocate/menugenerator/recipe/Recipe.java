9135b961062c528f92ec73678ed71a0c014ab7df
katelocate <katelocat3@gmail.com>
2024-06-15 18:38:44 +0200

    Add Recipe title constraint.

--- src/main/java/com/katelocate/menugenerator/recipe/Recipe.java
+++ src/main/java/com/katelocate/menugenerator/recipe/Recipe.java
@@ -1,12 +1,25 @@
 package com.katelocate.menugenerator.recipe;
 
+import jakarta.validation.constraints.NotEmpty;
+
 public record Recipe(
         Integer id,
+        @NotEmpty
         String title,
         RecipeType recipeType,
         String body
 ){
     public Recipe {
-//        if(!RecipeType)
+//        boolean isRightType = false;
+//        for (RecipeType type : RecipeType.values()) {
+//            if (type == recipeType) {
+//                isRightType = true;
+//                break;
+//            }
+//        }
+//        if (!isRightType) {
+//            throw new IllegalArgumentException("Choose existing recipe type.");
+//        }
+        
     }
 }
