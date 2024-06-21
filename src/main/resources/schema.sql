f21d578afae87500be3e47fe6658b1a1d0d0515b
katelocate <katelocat3@gmail.com>
2024-06-21 17:31:18 +0200

    Add recipeType to the Recipe table.

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,6 +1,7 @@
 CREATE TABLE IF NOT EXISTS Recipe (
    id INT NOT NULL,
    title VARCHAR(250) NOT NULL,
-   body VARCHAR(max) NOT NULL,
+   recipeType VARCHAR(50) NOT NULL,
+   body VARCHAR(max),
    PRIMARY KEY (id)
 );
