97b859b73919e0000c5019705347288d215ef4e4
katelocate <katelocat3@gmail.com>
2024-06-21 17:33:04 +0200

    Modify findAll method of the RecipeRepository class. Remove currently not used methods.

--- src/main/java/com/katelocate/menugenerator/recipe/RecipeRepository.java
+++ src/main/java/com/katelocate/menugenerator/recipe/RecipeRepository.java
@@ -1,47 +1,25 @@
 package com.katelocate.menugenerator.recipe;
 
-import jakarta.annotation.PostConstruct;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.jdbc.core.simple.JdbcClient;
 import org.springframework.stereotype.Repository;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
+
 
 @Repository
 public class RecipeRepository {
 
-    private List<Recipe> recipes = new ArrayList<>();
-
-    List<Recipe> findAll() {
-        return recipes;
+    private static final Logger log = LoggerFactory.getLogger(RecipeRepository.class);
+    private final JdbcClient jdbcClient;
+    public RecipeRepository(JdbcClient jdbcClient) {
+        this.jdbcClient = jdbcClient;
+    }
+    public List<Recipe> findAll() {
+        return jdbcClient.sql("select * from recipe")
+                .query(Recipe.class)
+                .list();
     }
 
-    Optional<Recipe> findById(Integer id){
-        return recipes.stream().filter(recipe -> recipe.id() == id).findFirst();
-    }
-
-    void create(Recipe recipe) {
-        recipes.add(recipe);
-    }
-
-    void update(Recipe recipe, Integer id) {
-        Optional<Recipe> existingRecipe = findById(id);
-        if (existingRecipe.isPresent()) {
-            recipes.set(recipes.indexOf(existingRecipe.get()), recipe);
-        }
-    }
-
-    void delete(Integer id) {
-        recipes.removeIf(recipe -> recipe.id().equals(id));
-    }
-
-    @PostConstruct
-    private void init() {
-        recipes.add(new Recipe(0, "Boiled Eggs", RecipeType.SNACK,
-                "Put an egg in a pot with cold water so that it covers an egg, wait until the water boils. " +
-                "Simmer for 8 minutes. Cool it down with running water in a sink."));
-        recipes.add(new Recipe(1, "Boiled", RecipeType.BREAKFAST,
-                "it covers an egg, wait until the water boils. Simmer for 8 minutes. Cool it down with running" +
-                        " water in a sink."));
-    }
 }
