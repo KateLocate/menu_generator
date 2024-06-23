dbf7e604457ab733f308024dd8abe2b03f58efbf
katelocate <katelocat3@gmail.com>
2024-06-23 08:36:05 +0200

    Add basic queries to db to the RecipeRepository.

--- src/main/java/com/katelocate/menugenerator/recipe/RecipeRepository.java
+++ src/main/java/com/katelocate/menugenerator/recipe/RecipeRepository.java
@@ -4,8 +4,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.jdbc.core.simple.JdbcClient;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.Assert;
 
 import java.util.List;
+import java.util.Optional;
 
 
 @Repository
@@ -13,13 +15,58 @@ public class RecipeRepository {
 
     private static final Logger log = LoggerFactory.getLogger(RecipeRepository.class);
     private final JdbcClient jdbcClient;
+
     public RecipeRepository(JdbcClient jdbcClient) {
         this.jdbcClient = jdbcClient;
     }
+
     public List<Recipe> findAll() {
-        return jdbcClient.sql("select * from recipe")
+        return jdbcClient.sql("SELECT * FROM Recipe")
                 .query(Recipe.class)
                 .list();
     }
 
+    public Optional<Recipe> findById(Integer id) {
+        return jdbcClient.sql("SELECT id, title, recipeType, body FROM Recipe WHERE id = :id")
+                .param("id", id)
+                .query(Recipe.class)
+                .optional();
+    }
+
+    public void create(Recipe recipe) {
+        var updated = jdbcClient.sql("INSERT INTO Recipe (id, title, recipeType, body) VALUES (?,?,?,?)")
+                .params(List.of(recipe.id(), recipe.title(), recipe.recipeType(), recipe.body().toString()))
+                .update();
+
+        Assert.state(updated == 1, "Failed to create recipe " + recipe.title());
+    }
+
+    public void update(Recipe recipe, Integer id) {
+        var updated = jdbcClient.sql("UPDATE Recipe SET title = ?, recipeType = ?, body = ? WHERE id = ?")
+                .params(List.of(recipe.title(), recipe.recipeType(), recipe.body(), id))
+                .update();
+
+        Assert.state(updated == 1, "Failed to update recipe " + recipe.title());
+    }
+
+    public void delete(Integer id) {
+        var updated = jdbcClient.sql("DELETE FROM Recipe WHERE id = :id")
+                .param("id", id)
+                .update();
+    }
+
+    public int count() {
+        return jdbcClient.sql("SELECT * FROM Recipe").query().listOfRows().size();
+    }
+
+    public void saveAll(List<Recipe> recipes) {
+        recipes.stream().forEach(this::create);
+    }
+
+    public List<Recipe> findByType(RecipeType recipeType) {
+        return jdbcClient.sql("SELECT * FROM Recipe WHERE recipeType = :recipeType")
+                .param("recipeType", recipeType)
+                .query(Recipe.class)
+                .list();
+    }
 }
