5ef2d34c2811200ae5ec80fe31c6d4aa9b44fc13
Kate <katelocat3@gmail.com>
2025-02-27 16:05:11 +0100

    Fix: remove commented code and unused imports.

--- src/main/java/com/katelocate/menugenerator/MenuGeneratorApplication.java
+++ src/main/java/com/katelocate/menugenerator/MenuGeneratorApplication.java
@@ -1,16 +1,11 @@
 package com.katelocate.menugenerator;
 
-import com.katelocate.menugenerator.user.User;
-import com.katelocate.menugenerator.user.UserRestClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.CommandLineRunner;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.stereotype.Component;
 
-import java.util.List;
 
 @SpringBootApplication
 public class MenuGeneratorApplication {
@@ -20,12 +15,4 @@ public class MenuGeneratorApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MenuGeneratorApplication.class, args);
 	}
-
-//	@Bean
-//	CommandLineRunner runner(UserRestClient client) {
-//		return args -> {
-//			List<User> users = client.findAll();
-//			System.out.println(users);
-//		};
-//	}
 }
