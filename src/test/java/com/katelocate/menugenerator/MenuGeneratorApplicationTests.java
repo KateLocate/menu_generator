a3cda19c04ca8f755077174781fbeb94d977e025
Kate <katelocat3@gmail.com>
2025-02-27 16:07:01 +0100

    Add test that checks if app starts.

--- src/test/java/com/katelocate/menugenerator/MenuGeneratorApplicationTests.java
+++ src/test/java/com/katelocate/menugenerator/MenuGeneratorApplicationTests.java
@@ -1,13 +1,21 @@
 package com.katelocate.menugenerator;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class MenuGeneratorApplicationTests {
 
+	@Autowired
+	private MenuGeneratorApplication app;
+
 	@Test
-	void contextLoads() {
+	void contextLoads() throws Exception {
+			assertThat(app).isNotNull();
 	}
 
 }
