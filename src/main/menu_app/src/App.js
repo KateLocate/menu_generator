09a467a94bfc9ef43b2203d21803bc3dbf8f9632
Kate <katelocat3@gmail.com>
2025-02-27 16:51:30 +0100

    Add a Router element with the base app routes.

--- src/main/menu_app/src/App.js
+++ src/main/menu_app/src/App.js
@@ -1,65 +1,26 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import Categories from "./components/Categories";
+import Search from "./components/Search";
+
 
 import './App.css';
 
 const App = () => {
-  const [recipes, setGroups] = useState([]);
-  const [loading, setLoading] = useState(false);
-
-  useEffect(() => {
-    setLoading(true);
-
-    fetch('api/recipes')
-      .then(response => response.json())
-      .then(data => {
-        setGroups(data);
-        setLoading(false);
-      })
-  }, []);
-
-  if (loading) {
-    return <p>Loading...</p>;
-  }
-
-  function Recipe({recipe, visible}) {
-    if (visible) {
-      return <p>{recipe.body}</p>;
-    }
-    return null;
-  }
-
-  function RecipeButton({recipe}) {
-    const [visible, setVisible] = useState(false);
-
-    function handleRecipeButtonClick() {
-      setVisible(!visible);
-      console.log(visible)
-    }
-
-    return (
-      <div>
-        <button className='recipeDetails' onClick={handleRecipeButtonClick}>
-          {recipe.title}
-        </button>
-        <p>
-          <Recipe visible={visible} recipe={recipe}/>
-        </p>
-      </div>
-    );
-  }
-
-
-
   return (
     <div className="app">
       <h1>Menu Generator</h1>
-      <div className='container'>
-        { recipes.map(recipe =>
-          <div className='recipe' key={recipe.id}>
-            <RecipeButton recipe={recipe} />
-          </div>
-        )};
-      </div>
+        <Router>
+          <Navbar className='navbar' />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/categories" element={<Categories />} />
+            <Route path="/search" element={<Search />} />
+          </Routes>
+        </Router>
     </div>
   );
 }
