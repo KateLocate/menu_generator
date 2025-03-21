515929485cc41777b76a0e132447c2aaf693d5ec
Kate <katelocat3@gmail.com>
2025-03-21 16:09:21 +0100

    Add: setup for API tests.

---
+++ src/test/java/com/katelocate/menugenerator/RecipeControllerTests.java
@@ -0,0 +1,79 @@
+package com.katelocate.menugenerator;
+
+import com.katelocate.menugenerator.recipe.RecipeRepository;
+
+import io.restassured.RestAssured;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+
+import org.testcontainers.containers.PostgreSQLContainer;
+
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class RecipeControllerTests {
+
+    @LocalServerPort
+    private Integer port;
+
+    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>(
+            "postgres:16-alpine"
+    );
+
+    static JdbcTemplate jdbcTemplate;
+
+    static void jdbcTemplateSetup() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setUrl(postgres.getJdbcUrl());
+        dataSource.setUsername(postgres.getUsername());
+        dataSource.setPassword(postgres.getPassword());
+        jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
+
+    @BeforeAll
+    static void beforeAll() {
+        postgres.start();
+        jdbcTemplateSetup();
+    }
+
+    @AfterAll
+    static void afterAll() {
+        postgres.stop();
+    }
+
+
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+    }
+
+    @Autowired
+    RecipeRepository recipeRepository;
+
+    @BeforeEach
+    void setUp() {
+        RestAssured.baseURI = "http://localhost:" + port;
+        recipeRepository.deleteAll();
+    }
+
+    @Test
+    void shouldFindAll() {
+
+    }
+}
