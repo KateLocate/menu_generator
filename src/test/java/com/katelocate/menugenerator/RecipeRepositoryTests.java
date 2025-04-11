4c46928684e18307ee4b51afe0ef37b0322d75d8
Kate <katelocat3@gmail.com>
2025-04-11 12:41:05 +0200

    Fix: Remove redundant import.

--- src/test/java/com/katelocate/menugenerator/RecipeRepositoryTests.java
+++ src/test/java/com/katelocate/menugenerator/RecipeRepositoryTests.java
@@ -13,7 +13,6 @@ import org.junit.jupiter.api.Test;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.server.LocalServerPort;
 
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
