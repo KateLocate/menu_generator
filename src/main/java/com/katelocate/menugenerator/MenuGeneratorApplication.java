f350da584dfefd14a48bea542a417972b5a7b11a
Kate <katelocat3@gmail.com>
2025-04-15 19:49:19 +0200

    Add: Generate random number and sequence of numbers using Random class.

--- src/main/java/com/katelocate/menugenerator/MenuGeneratorApplication.java
+++ src/main/java/com/katelocate/menugenerator/MenuGeneratorApplication.java
@@ -6,13 +6,40 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Random;
+import java.util.Set;
+
 
 @SpringBootApplication
 public class MenuGeneratorApplication {
 
-	private static final Logger logger = LoggerFactory.getLogger(MenuGeneratorApplication.class);
+    private static final Logger logger = LoggerFactory.getLogger(MenuGeneratorApplication.class);
 
-	public static void main(String[] args) {
-		SpringApplication.run(MenuGeneratorApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MenuGeneratorApplication.class, args);
+
+        int[] arr = {11, 12, 13, 14, 18, 49, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
+
+        Random random = new Random();
+        int randomIndex = random.nextInt(arr.length);
+
+        logger.info(String.format("Random int index: %d value: %d", randomIndex, arr[randomIndex]));
+
+        Set<Integer> dayRecipeIndexes = new java.util.HashSet<>(Set.of(random.nextInt(arr.length)));
+        int recipesNeeded = 3;
+        int counter = 0;
+        logger.info("Random day recipe: " + dayRecipeIndexes);
+
+
+        while (counter < recipesNeeded) {
+            randomIndex = random.nextInt(arr.length);
+            if (!dayRecipeIndexes.contains(randomIndex)) {
+                dayRecipeIndexes.add(randomIndex);
+                counter++;
+            }
+        }
+
+        logger.info("Random day recipes: " + dayRecipeIndexes);
+
+    }
 }
