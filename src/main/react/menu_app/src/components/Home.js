3766f8119322b0f9309b3d1b15709a43c42ef6b7
Kate <katelocat3@gmail.com>
2025-05-04 16:53:42 +0200

    Add: DisplayRecipe, DisplayDayRecipes, DisplayMenu components.
    Why: To reuse code, improve readability, address issues with collapse feature.

--- src/main/react/menu_app/src/components/Home.js
+++ src/main/react/menu_app/src/components/Home.js
@@ -33,18 +33,63 @@ function Home() {
       });
   }
 
-  function RecipeButton ({recipe}) {
+  function RecipeButton ({index="n", recipe}) {
     return (
         <button className="btn btn-primary"
               type="button"
               data-bs-toggle="collapse"
-              data-bs-target={`#collapse-${recipe.id}`}
+              data-bs-target={`#collapse-${index+"-"+recipe.id}`}
               aria-expanded="false"
-              aria-controls={`collapse-${recipe.id}`}>
+              aria-controls={`collapse-${index+"-"+recipe.id}`}>
           {recipe.title}
         </button>
     );
   }
+
+  function DisplayRecipe ({index="n", recipe}) {
+    return (
+        <div className="container mt-3" key={`${index+"-"+recipe.id}`}>
+          <p className="d-inline-flex gap-1" >
+            <RecipeButton recipe={recipe} index={index} />
+          </p>
+          <div className="collapse" id={`collapse-${index+"-"+recipe.id}`}>
+            <div className="card card-body">
+              {recipe.body}
+              {console.log(index+" "+recipe.id)}
+            </div>
+          </div>
+        </div>
+    )
+  }
+
+  function DisplayDayRecipes ({index="n", recipes}) {
+    return (
+        <div key={index}>
+          <li>
+            { recipes.map((recipe) => {
+              return (<DisplayRecipe index={index} recipe={recipe} />)
+              })
+            }
+          </li>
+        </div>
+    )
+  }
+
+  function DisplayMenu() {
+    return (
+        <div>
+          {loading && <p>Loading...</p>}
+          <ol>
+            { weekRecipes.map((dayRecipes, index) => {
+              return (
+                  <DisplayDayRecipes index={index} recipes={dayRecipes} />
+              )
+            })}
+          </ol>
+        </div>
+    )
+  }
+
 
   return (
     <div className="container">
@@ -52,42 +97,16 @@ function Home() {
         <div className="col">
           <button type="button" className="btn btn-outline-primary btn-lg" onClick={fetchDayRecipes}>Generate day menu</button>
           {loading && <p>Loading...</p>}
-          { dayRecipes.map(recipe =>
-          <div className="container mt-3" key={recipe.id}>
-            <p className="d-inline-flex gap-1" >
-              <RecipeButton recipe={recipe} />
-            </p>
-            <div className="collapse" id={`collapse-${recipe.id}`}>
-              <div className="card card-body">
-                {recipe.body}
-              </div>
-            </div>
-          </div>
+          { dayRecipes.map(recipe => {
+              return(
+                  <DisplayRecipe recipe={recipe} />
+              ) }
           ) }
         </div>
 
         <div className="col">
           <button type="button" className="btn btn-outline-primary btn-lg" onClick={fetchWeekRecipes}>Generate week menu</button>
-          {loading && <p>Loading...</p>}
-          <ol>
-          { weekRecipes.map((weekDayRecipes) => {
-            return (<div><li>
-              { weekDayRecipes.map((recipe) => {
-                return (<div className="container mt-3" key={recipe.id}>
-                      <p className="d-inline-flex gap-1" >
-                        <RecipeButton recipe={recipe} />
-                      </p>
-                      <div className="collapse" id={`collapse-${recipe.id}`}>
-                        <div className="card card-body">
-                          {recipe.body}
-                        </div>
-                      </div>
-                    </div>)
-                })
-              }
-              </li></div>)
-            })}
-            </ol>
+          <DisplayMenu />
         </div>
       </div>
     </div>
