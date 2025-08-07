6f7ce5bcf0453f24907fe1056b72ddf813e59958
Kate <katelocat3@gmail.com>
2025-08-07 14:02:37 +0200

    Add Spring boot csrf protection.

--- backend/src/main/java/com/katelocate/menugenerator/MenuGeneratorApplication.java
+++ backend/src/main/java/com/katelocate/menugenerator/MenuGeneratorApplication.java
@@ -3,6 +3,9 @@ package com.katelocate.menugenerator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -11,6 +14,20 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MenuGeneratorApplication {
 
 	private static final Logger logger = LoggerFactory.getLogger(MenuGeneratorApplication.class);
+
+	@EnableWebSecurity
+	public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http
+				.csrf().disable()
+				.authorizeRequests()
+				.antMatchers("/api/**").permitAll()
+				.anyRequest().authenticated();
+		}
+	}
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(MenuGeneratorApplication.class, args);
