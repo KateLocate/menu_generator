563c5764457320a00d711b92ea66318f35c3ea15
Kate <katelocat3@gmail.com>
2025-09-11 17:27:22 +0200

    Update Recipe properties.

--- backend/src/main/java/com/katelocate/menugenerator/recipe/Recipe.java
+++ backend/src/main/java/com/katelocate/menugenerator/recipe/Recipe.java
@@ -2,10 +2,13 @@ package com.katelocate.menugenerator.recipe;
 
 import jakarta.validation.constraints.NotEmpty;
 
+import java.util.List;
+
 public record Recipe(
         Integer id,
         @NotEmpty
         String title,
         RecipeType recipeType,
-        String body
+        List<String> ingredients,
+        List<String> instructions
 ){};
