cc79b880eaf877f8486b291cea3d9df8a405bea8
Kate <katelocat3@gmail.com>
2025-09-11 17:26:31 +0200

    Update db schema.

--- backend/src/main/resources/schema.sql
+++ backend/src/main/resources/schema.sql
@@ -2,6 +2,8 @@ CREATE TABLE IF NOT EXISTS Recipe (
    id INT NOT NULL,
    title VARCHAR(250) NOT NULL,
    recipeType VARCHAR(50) NOT NULL,
-   body TEXT,
+   ingredients TEXT[],
+   instructions TEXT[],
    PRIMARY KEY (id)
 );
+
