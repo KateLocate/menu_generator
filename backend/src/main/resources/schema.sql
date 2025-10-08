d366c79bbc51b45ca4c06a467d7a30560d71c3f1
Kate <katelocat3@gmail.com>
2025-10-08 19:45:22 +0200

    Update schema.

--- backend/src/main/resources/schema.sql
+++ backend/src/main/resources/schema.sql
@@ -1,9 +1,10 @@
+DROP TABLE IF EXISTS Recipe;
 CREATE TABLE IF NOT EXISTS Recipe (
    id INT NOT NULL,
    title VARCHAR(250) NOT NULL,
-   recipeType VARCHAR(50) NOT NULL,
-   ingredients TEXT,
-   instructions TEXT,
+   recipe_type VARCHAR(50) NOT NULL,
+   ingredients TEXT[],
+   instructions TEXT[],
    PRIMARY KEY (id)
 );
 
