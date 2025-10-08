1b5a0562b91eb34dba27ba812ba044f1973b79f2
Kate <katelocat3@gmail.com>
2025-10-08 19:47:36 +0200

    Make Recipe POJO with persistence.Entity annotation.

--- backend/src/main/java/com/katelocate/menugenerator/recipe/Recipe.java
+++ backend/src/main/java/com/katelocate/menugenerator/recipe/Recipe.java
@@ -1,14 +1,43 @@
 package com.katelocate.menugenerator.recipe;
 
-import jakarta.validation.constraints.NotEmpty;
-
 import java.util.List;
 
-public record Recipe(
-        Integer id,
-        @NotEmpty
-        String title,
-        RecipeType recipeType,
-        String ingredients,
-        String instructions
-){};
+import lombok.Getter;
+import lombok.Setter;
+import lombok.AllArgsConstructor;
+
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.Column;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Id;
+
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@Entity
+@Table(name = "recipe")
+public class Recipe {
+    @Id
+    public Integer id;
+
+    @NotEmpty
+    public String title;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "recipe_type")
+    public RecipeType recipeType;
+
+    @JdbcTypeCode(SqlTypes.ARRAY)
+    @Column(columnDefinition = "text[]")
+    public List<String> instructions;
+
+    @JdbcTypeCode(SqlTypes.ARRAY)
+    @Column(columnDefinition = "text[]")
+    public List<String> ingredients;
+};
